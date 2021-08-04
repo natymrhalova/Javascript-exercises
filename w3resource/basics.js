c9eedef67f746eeaa30707ab31ac5ef2e3bffbd8
natymrhalova <naty.mrhalova@gmail.com>
2021-08-04 14:12:14 +0200

    Exercise 4 done neeeds a lot of style and error handling

--- w3resource/basics.js
+++ w3resource/basics.js
@@ -1,8 +1,10 @@
+//Exercises sourced from https://www.w3resource.com/javascript-exercises/javascript-basic-exercises.php
+
 /*1. Write a JavaScript program to display the current day and time in the following format.Go to the editor
 Sample Output: Today is: Tuesday.
 Current time is: 10 PM: 30 : 38*/
 
-let date = new Date();
+const date = new Date();
 let day = date.getDay() - 1;
 let hours = date.getHours();
 let minutes = date.getMinutes();
@@ -16,7 +18,7 @@ console.log(`Current time : ${hours} ${appended_value} : ${minutes} : ${seconds}
 
 /*2. Write a JavaScript program where the program takes a random integer between 1 to 10, the user is then prompted to input a guess number. If the user input matches with guess number, the program will display a message "Good Work" otherwise display a message "Not matched". */
 
-let button = document.querySelector("button[type=submit]")
+let button = document.querySelector("#submit")
 let reset = document.querySelector("button[type=reset]")
 
 button.addEventListener("click", () => {
@@ -29,8 +31,9 @@ reset.addEventListener("click", () => {
     document.getElementById("number").value = "";
 })
 
+//New date variable created solely for the purpose of an exercise number 3
 //3. Write a JavaScript program to calculate days left until next Christmas.
-let another_date = new Date();
+const another_date = new Date();
 let today = another_date.getTime();
 let another_day = another_date.getDate();
 let another_month = another_date.getMonth() + 1;
@@ -47,11 +50,37 @@ let christmas_time = christmas.getTime();
 
 let seconds_left = today - christmas_time; scroll
 
-var seconds_to_days = 1000 * 60 * 60 * 24;
+let seconds_to_days = 1000 * 60 * 60 * 24;
 
 let days_left_to_christmas = Math.abs(Math.round(seconds_left / seconds_to_days))
 
 console.log(`${days_left_to_christmas} days till Christmas`)
+
+/*Write a JavaScript program to convert temperatures to and from Celsius, Fahrenheit.Go to the editor
+[Formula : c / 5 = (f - 32) / 9[where c = temperature in Celsius and f = temperature in Fahrenheit ]
+Expected Output:
+60°C is 140 °F
+45°F is 7.222222222222222°C*/
+
+const temperature_converter_button = document.getElementById("temperature-submit-button")
+
+temperature_converter_button.addEventListener("click", () => {
+    let celsius_input = document.getElementById("celsius").value;
+    let fahrenheit_input = document.getElementById("fahrenheit").value
+
+    let fahrenheit_output = ((9 * celsius_input) + (32 * 5)) / 5
+
+    let celsius_output = (5 * (fahrenheit_input - 32)) / 9
+
+    document.getElementById("celsius-to-fahrenheit").textContent = `Your degrees of Celsius (${celsius_input} °C) are this much ${fahrenheit_output} in Fahrenheit`
+
+    document.getElementById("fahrenheit-to-celsius").textContent = `Your degrees of Fahrenheit (${fahrenheit_input} °F) are ${celsius_output} are this much in Celsius degrees`
+
+})
+
+
+
+
 
 
 
