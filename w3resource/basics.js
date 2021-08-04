775ce0f8f7c422d1fd89c2f8280f325aaf4654cb
natymrhalova <naty.mrhalova@gmail.com>
2021-08-04 11:22:42 +0200

    Exercise 2 user input checks if its the same as random number

--- w3resource/basics.js
+++ w3resource/basics.js
@@ -2,14 +2,38 @@
 Sample Output: Today is: Tuesday.
 Current time is: 10 PM: 30 : 38*/
 
-var date = new Date();
-var day = date.getDay() - 1;
-var hours = date.getHours();
-var minutes = date.getMinutes();
-var seconds = date.getSeconds();
-var days_array = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
-var appended_value = (hours >= 12) ? " PM" : " AM";
+let date = new Date();
+let day = date.getDay() - 1;
+let hours = date.getHours();
+let minutes = date.getMinutes();
+let seconds = date.getSeconds();
+let days_array = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+let appended_value = (hours >= 12) ? " PM" : " AM";
 hours = (hours >= 12) ? hours - 12 : hours;
 
 console.log(`Today is ${days_array[day]}`);
 console.log(`Current time : ${hours} ${appended_value} : ${minutes} : ${seconds}`);
+
+/*Write a JavaScript program where the program takes a random integer between 1 to 10, the user is then prompted to input a guess number. If the user input matches with guess number, the program will display a message "Good Work" otherwise display a message "Not matched". */
+
+let button = document.querySelector("button[type=submit]")
+let reset = document.querySelector("button[type=reset]")
+
+button.addEventListener("click", () => {
+    let input_value = document.getElementById("number").value;
+    let random_number = Math.round(Math.random() * 11)
+        (input_value === random_number) ? alert("Good work!") : alert("Not matched")
+})
+
+reset.addEventListener("click", () => {
+    document.getElementById("number").value = "";
+})
+
+
+
+
+
+
+
+
+
